77962c321b8dd23a7077753913ad92d094ba7d41
Vano Devium <webdevium@gmail.com>
2020-04-22 23:00:51 +0300

    Added condition for full compliance with the math algorithm

--- src/luhn.js
+++ src/luhn.js
@@ -8,7 +8,7 @@ module.exports = (function(){
 			, calc
 			, calc2;
 
-			if (!/^[0-9]+$/.test(trimmed)) {
+			if (!/^[\d]+$/.test(trimmed)) {
 				return false;
 			}
 
@@ -18,21 +18,15 @@ module.exports = (function(){
 					total += calc;
 				} else {
 					calc2 = calc * 2;
-
-					switch (calc2) {
-					case 10: calc2 = 1; break;
-					case 12: calc2 = 3; break;
-					case 14: calc2 = 5; break;
-					case 16: calc2 = 7; break;
-					case 18: calc2 = 9; break;
-					default: calc2 = calc2;
+					if (calc2 > 9) {
+						calc2 -= 9;
 					}
 					total += calc2;
 				}
 				odd = !odd;
 			}
 
-			return (total !== 0 && (total % 10) === 0);
+			return total !== 0 && (total % 10) === 0;
 		}
 
 		return {
